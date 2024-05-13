d1be2bc22ebd829ef1debb5bdf5946b358e121de
yassin ahmed <yassin.ahmed3070@gmail.com>
2024-05-13 23:58:59 +0300

    v2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ __('index.Rawasi') }} | {{ __('dashboard.Dashboard') }}</title>
+    <title> {{ __('dashboard.Dashboard') }}</title>
     
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
